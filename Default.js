bd32c87514ad3f5d254ae400801a1f99e7d06629
Akiicat <aaaa1379@gmail.com>
2026-02-21 13:53:50 +0800

    first commit

---
+++ Default.js
@@ -0,0 +1,24 @@
+/**
+ * Default: Registers default GET and POST handlers for the AppBootstrap framework.
+ */
+
+/**
+ * Default Route Not Found handler.
+ */
+function RouteNotFound(req) {
+    return Response.json({
+        status: false,
+        message: 'Action not found (and debug handler default missing)',
+        request: req,
+    });
+}
+/**
+ * Default GET handler.
+ */
+AppBootstrap.registerDefaultGet(RouteNotFound);
+
+/**
+ * Default POST handler.
+ */
+AppBootstrap.registerDefaultPost(RouteNotFound);
+
